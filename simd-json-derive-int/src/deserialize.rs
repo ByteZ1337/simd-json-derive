d3481178d007ec87a0150f2e71753a63b1266d4a
ByteZ1337 <0xbytez@gmail.com>
2024-04-05 00:58:23 +0200

    Rework named struct derive macro
    Avoids creating tons of Vecs that may never even be used by generating fields code fragments beforehand and storing the tokens in fewer Vecs

--- simd-json-derive-int/src/deserialize.rs
+++ simd-json-derive-int/src/deserialize.rs
@@ -10,65 +10,93 @@ use syn::{
 
 use crate::args::*;
 
+fn is_option(field: &Field) -> bool {
+    if let Type::Path(TypePath {
+        path: Path { segments, .. },
+        ..
+    }) = &field.ty
+    {
+        if let Some(PathSegment { ident, .. }) = segments.first() {
+            return ident == "Option";
+        }
+    }
+    false
+}
+
 /// Named struct as `Struct(u8)` or `Struct(u8, String)`
 fn derive_named_struct(
     attrs: StructAttrs,
     ident: Ident,
     generics: Generics,
     fields: Punctuated<Field, Comma>,
-) -> proc_macro::TokenStream {
-    let mut value_keys = Vec::new();
-    let mut value_locals = Vec::new();
-    let mut values = Vec::new();
-
-    let mut default_keys = Vec::new();
-    let mut default_locals = Vec::new();
-    let mut defaults = Vec::new();
-    let mut default_types = Vec::new();
-
-    let mut options = Vec::new();
-    let mut option_locals = Vec::new();
-    let mut option_keys = Vec::new();
-
-    let deny_unknown_fields: bool = attrs.deny_unknown_fields();
+) -> TokenStream {
     let params = &generics.params;
     let (all_generics, derive_lt) = match params.first() {
         None => (quote! { <'input> }, quote! { 'input }),
         Some(GenericParam::Lifetime(lifetime)) => (quote! { <#params> }, quote! { #lifetime }),
         Some(_) => (quote! { <'input, #params> }, quote! { 'input }),
     };
-    for (id, f) in fields.iter().enumerate() {
-        let mut is_option = false;
-        if let Type::Path(TypePath {
-            path: Path { segments, .. },
-            ..
-        }) = &f.ty
-        {
-            if let Some(PathSegment { ident, .. }) = segments.first() {
-                is_option = ident == "Option";
-            }
-        }
 
+    let mut names: Vec<String> = Vec::new();
+    let mut locals = Vec::new();
+    let mut deserialize_fields = Vec::new();
+    let mut unwrap_fields = Vec::new();
+
+    for (id, f) in fields.iter().enumerate() {
+        let is_option = is_option(f);
+        let is_default = attrs.default_fallback(f);
         let ident = f.ident.clone().expect("Missing ident");
+        let type_ident = &f.ty;
+
         let name = attrs.name(f);
         let name = name.trim_matches(':').trim_matches('"').to_string();
-        if is_option {
-            options.push(ident);
-            option_locals.push(format_ident!("__option_{}", id));
-            option_keys.push(name);
+        names.push(name.clone());
+
+        let local = format_ident!("__value_{}", id);
+        locals.push(local.clone());
+
+        let deserialize_field = if is_option {
+            quote! {
+                #local = ::simd_json_derive::Deserialize::from_tape(__deser_tape)?;
+            }
         } else {
-            if attrs.default_fallback(f) {
-                default_keys.push(name);
-                default_locals.push(format_ident!("__default_{}", id));
-                defaults.push(ident);
-                default_types.push(&f.ty);
-            } else {
-                value_keys.push(name);
-                value_locals.push(format_ident!("__value_{}", id));
-                values.push(ident);
+            quote! {
+                let v = ::simd_json_derive::Deserialize::from_tape(__deser_tape)?;
+                #local = Some(v);
+            }
+        };
+        deserialize_fields.push(deserialize_field);
+
+        let unwrap_field = if is_option {
+            quote! {
+                #ident: #local,
+            }
+        } else if is_default {
+            quote! {
+                #ident: #local.unwrap_or_else(|| <#type_ident>::default()),
+            }
+        } else {
+            quote! {
+                #ident: #local.ok_or_else(|| ::simd_json::Error::custom(format!("missing field: `{}`", #name)))?,
+            }
+        };
+        unwrap_fields.push(unwrap_field);
+    }
+
+    let deny_unknown_fields = if attrs.deny_unknown_fields() {
+        quote! {
+            __unknown_field => {
+                return Err(::simd_json::Error::unknown_field(__unknown_field, &[ #(#names,)* ]));
             }
         }
-    }
+    } else {
+        quote! {
+            _ => {
+                // ignore unknown field
+                ::simd_json_derive::__skip(1, __deser_tape)
+            }
+        }
+    };
 
     let expanded = quote! {
         impl #all_generics ::simd_json_derive::Deserialize <#derive_lt> for #ident #generics {
@@ -86,38 +114,18 @@ fn derive_named_struct(
                     return Err(::simd_json::Error::generic(::simd_json::ErrorType::ExpectedMap));
                 };
 
-                #(let mut #value_locals = None;)*
-                #(let mut #default_locals = None;)*
-                #(let mut #option_locals = None;)*
+                #(let mut #locals = None;)*
 
                 for _ in 0..__deser_len {
                     match __deser_tape.next() {
                         Some(::simd_json::Node::String(__deser_key)) =>  {
                             match __deser_key {
                                 #(
-                                #value_keys => {
-                                    let v = ::simd_json_derive::Deserialize::from_tape(__deser_tape)?;
-                                    #value_locals = Some(v);
+                                #names => {
+                                    #deserialize_fields
                                 }
                                 )*
-                                #(
-                                #default_keys => {
-                                    let v = ::simd_json_derive::Deserialize::from_tape(__deser_tape)?;
-                                    #default_locals = Some(v);
-                                }
-                                )*
-                                #(
-                                #option_keys => {
-                                    #option_locals = ::simd_json_derive::Deserialize::from_tape(__deser_tape)?;
-                                }
-                                )*
-                                __unknown_field if #deny_unknown_fields => {
-                                    return Err(::simd_json::Error::unknown_field(__unknown_field, &[ #(#value_keys,)* #(#option_keys,)* ]));
-                                }
-                                _ => {
-                                    // ignore unknown field
-                                    ::simd_json_derive::__skip(1, __deser_tape)
-                                }
+                                #deny_unknown_fields
                             }
                         },
                         // There are no more elements
@@ -125,19 +133,14 @@ fn derive_named_struct(
                     }
                 }
                 Ok(#ident {
-                        #(
-                            #options: #option_locals,
-                        )*
-                        #(
-                            #defaults: #default_locals.unwrap_or_else(|| <#default_types>::default()),
-                        )*
-                        #(
-                            #values: #value_locals.ok_or_else(|| ::simd_json::Error::custom(format!("missing field: `{}`", #value_keys)))?,
-                        )*
+                    #(
+                        #unwrap_fields
+                    )*
                 })
             }
         }
     };
+
     TokenStream::from(expanded)
 }
 
